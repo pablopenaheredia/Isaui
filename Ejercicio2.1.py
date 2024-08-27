544d8971cd7582143984bd415949549adb9d3e78
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2024-08-27 10:06:38 -0300

    Update Ejercicio2.1.py
    update con grid

--- Ejercicio2.1.py
+++ Ejercicio2.1.py
@@ -6,41 +6,41 @@ class Calculadora:
         self.raiz.title("Calculadora")
 
         self.etiqueta_num1 = tk.Label(self.raiz, text="Primer número")
-        self.etiqueta_num1.pack()
+        self.etiqueta_num1.grid(row=0, column=0, padx=5, pady=5)
 
-        self.entry_num1 = tk.Entry(self.raiz, width=10)
-        self.entry_num1.pack()
+        self.entry_num1 = tk.Entry(self.raiz, width=20)
+        self.entry_num1.grid(row=0, column=1, padx=5, pady=5)
 
         self.etiqueta_num2 = tk.Label(self.raiz, text="Segundo número")
-        self.etiqueta_num2.pack()
+        self.etiqueta_num2.grid(row=1, column=0, padx=5, pady=5)
 
-        self.entry_num2 = tk.Entry(self.raiz, width=10)
-        self.entry_num2.pack()
+        self.entry_num2 = tk.Entry(self.raiz, width=20)
+        self.entry_num2.grid(row=1, column=1, padx=5, pady=5)
 
         self.etiqueta_resultado = tk.Label(self.raiz, text="Resultado")
-        self.etiqueta_resultado.pack()
+        self.etiqueta_resultado.grid(row=2, column=0, padx=5, pady=5)
 
-        self.entry_resultado = tk.Entry(self.raiz, width=10)
+        self.entry_resultado = tk.Entry(self.raiz, width=20)
         self.entry_resultado.config(state="readonly")
-        self.entry_resultado.pack()
+        self.entry_resultado.grid(row=2, column=1, padx=5, pady=5)
 
-        self.boton_suma = tk.Button(self.raiz, text="+", command=lambda: self.operacion("+"))
-        self.boton_suma.pack(side=tk.LEFT)
+        self.boton_suma = tk.Button(self.raiz, text="+", command=lambda: self.operacion("+"), width=10)
+        self.boton_suma.grid(row=3, column=0, padx=5, pady=5)
 
-        self.boton_resta = tk.Button(self.raiz, text="-", command=lambda: self.operacion("-"))
-        self.boton_resta.pack(side=tk.LEFT)
+        self.boton_multiplicacion = tk.Button(self.raiz, text="*", command=lambda: self.operacion("*"), width=10)
+        self.boton_multiplicacion.grid(row=4, column=0, padx=5, pady=5)
 
-        self.boton_multiplicacion = tk.Button(self.raiz, text="*", command=lambda: self.operacion("*"))
-        self.boton_multiplicacion.pack(side=tk.LEFT)
+        self.boton_porcentaje = tk.Button(self.raiz, text="%", command=lambda: self.operacion("%"), width=10)
+        self.boton_porcentaje.grid(row=5, column=0, padx=5, pady=5)
 
-        self.boton_division = tk.Button(self.raiz, text="/", command=lambda: self.operacion("/"))
-        self.boton_division.pack(side=tk.LEFT)
+        self.boton_resta = tk.Button(self.raiz, text="-", command=lambda: self.operacion("-"), width=10)
+        self.boton_resta.grid(row=3, column=1, padx=5, pady=5)
 
-        self.boton_porcentaje = tk.Button(self.raiz, text="%", command=lambda: self.operacion("%"))
-        self.boton_porcentaje.pack(side=tk.LEFT)
+        self.boton_division = tk.Button(self.raiz, text="/", command=lambda: self.operacion("/"), width=10)
+        self.boton_division.grid(row=4, column=1, padx=5, pady=5)
 
-        self.boton_reset = tk.Button(self.raiz, text="RESET", command=self.reset)
-        self.boton_reset.pack(side=tk.LEFT)
+        self.boton_reset = tk.Button(self.raiz, text="Limpiar", command=self.reset, width=20)
+        self.boton_reset.grid(row=5, column=1, padx=5, pady=5)
 
         self.raiz.mainloop()
 
