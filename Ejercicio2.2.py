1a66c7722a676d3cb3fbda30afb25ecff04ecd49
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2024-08-27 10:11:19 -0300

    Update Ejercicio2.2.py
    correcion de peliculas

--- Ejercicio2.2.py
+++ Ejercicio2.2.py
@@ -5,20 +5,28 @@ class Peliculas:
         self.raiz = tk.Tk()
         self.raiz.title("Películas")
 
-        self.etiqueta_titulo = tk.Label(self.raiz, text="Escribe el título de una película")
-        self.etiqueta_titulo.pack()
+        # Columna 1
+        self.frame_izquierda = tk.Frame(self.raiz)
+        self.frame_izquierda.grid(row=0, column=0, padx=10, pady=10)
 
-        self.entry_titulo = tk.Entry(self.raiz, width=20)
-        self.entry_titulo.pack()
+        self.etiqueta_titulo = tk.Label(self.frame_izquierda, text="Escribe el título de una película")
+        self.etiqueta_titulo.grid(row=0, column=0, padx=5, pady=5)
 
-        self.etiqueta_peliculas = tk.Label(self.raiz, text="Películas")
-        self.etiqueta_peliculas.pack()
+        self.entry_titulo = tk.Entry(self.frame_izquierda, width=20)
+        self.entry_titulo.grid(row=1, column=0, padx=5, pady=5)
 
-        self.listbox_peliculas = tk.Listbox(self.raiz, width=20)
-        self.listbox_peliculas.pack()
+        self.boton_anadir = tk.Button(self.frame_izquierda, text="Añadir", command=self.anadir_pelicula)
+        self.boton_anadir.grid(row=2, column=0, padx=5, pady=5)
 
-        self.boton_anadir = tk.Button(self.raiz, text="Añadir", command=self.anadir_pelicula)
-        self.boton_anadir.pack()
+        # Columna 2
+        self.frame_derecha = tk.Frame(self.raiz)
+        self.frame_derecha.grid(row=0, column=1, padx=10, pady=10)
+
+        self.etiqueta_peliculas = tk.Label(self.frame_derecha, text="Películas")
+        self.etiqueta_peliculas.grid(row=0, column=0, padx=5, pady=5)
+
+        self.listbox_peliculas = tk.Listbox(self.frame_derecha, width=20)
+        self.listbox_peliculas.grid(row=1, column=0, padx=5, pady=5)
 
         self.raiz.mainloop()
 
