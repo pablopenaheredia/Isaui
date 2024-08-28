df3674ae5bedd41cd488453fe8a07d0a82f6d5b6
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2024-08-27 23:41:43 -0300

    Update Ejercicio1.2.py

--- Ejercicio1.2.py
+++ Ejercicio1.2.py
@@ -2,23 +2,23 @@ import tkinter as tk
 
 class ContDecreciente:
     def __init__(self):
-        self.raiz = tk.Tk()
-        self.raiz.title("Contador Decreciente")
+        self.ventana = tk.Tk()
+        self.ventana.title("Contador Decreciente")
         self.contador = 88
 
         # Creamos los widgets
-        self.etiqueta = tk.Label(self.raiz, text="Contador")
-        self.entry = tk.Entry(self.raiz, width=10)
+        self.etiqueta = tk.Label(self.ventana, text="Contador")
+        self.entry = tk.Entry(self.ventana, width=10)
         self.entry.insert(0, "88")
         self.entry.config(state="readonly")
-        self.boton = tk.Button(self.raiz, text="-", command=self.decrementar)
+        self.boton = tk.Button(self.ventana, text="-", command=self.decrementar)
 
         # Los colocamos en la ventana utilizando grid
         self.etiqueta.grid(row=0, column=0, padx=5, pady=5)
         self.entry.grid(row=0, column=1, padx=5, pady=5)
         self.boton.grid(row=0, column=2, padx=5, pady=5)
 
-        self.raiz.mainloop()
+        self.ventana.mainloop()
 
     def decrementar(self):
         self.contador -= 1
