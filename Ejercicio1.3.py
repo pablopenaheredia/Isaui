57abbf11d38f994b20c2aa35f397bc17f7d63827
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2024-09-02 03:13:25 -0300

    Update Ejercicio1.3.py

--- Ejercicio1.3.py
+++ Ejercicio1.3.py
@@ -1,16 +1,25 @@
 import tkinter as tk
 
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2, x+1):
+            resultado *= i
+        return resultado
+
 class Factorial:
     def __init__(self):
         self.ventana = tk.Tk()
         self.ventana.title("Factorial")
         self.n = 2
 
-        self.etiqueta_n = tk.Label(self.ventana, text="n")
-        self.etiqueta_n.grid(row=0, column=0, padx=5, pady=5)
+        self.titulo = tk.Label(self.ventana, text="n")
+        self.titulo.grid(row=0, column=0, padx=5, pady=5)
 
-        self.etiqueta_factorial = tk.Label(self.ventana, text="Factorial(n)")
-        self.etiqueta_factorial.grid(row=0, column=2, padx=5, pady=5)
+        self.titulo_factorial = tk.Label(self.ventana, text="Factorial(n)")
+        self.titulo_factorial.grid(row=0, column=2, padx=5, pady=5)
 
         self.entry_n = tk.Entry(self.ventana, width=10)
         self.entry_n.insert(0, "2")
@@ -22,24 +31,22 @@ class Factorial:
         self.entry_factorial.config(state="readonly")
         self.entry_factorial.grid(row=0, column=3, padx=5, pady=5)
 
-        self.boton = tk.Button(self.ventana, text="Siguiente", command=self.siguiente)
+        self.boton = tk.Button(self.ventana, text="Siguiente", command=self.boton_siguiente)
         self.boton.grid(row=1, column=0, columnspan=4, padx=5, pady=5)
 
         self.ventana.mainloop()
 
-    def siguiente(self):
+    def boton_siguiente(self):
         self.n += 1
         self.entry_n.config(state="normal")
         self.entry_n.delete(0, tk.END)
         self.entry_n.insert(0, str(self.n))
         self.entry_n.config(state="readonly")
 
-        factorial = 1
-        for i in range(1, self.n + 1):
-            factorial *= i
+        resultado_factorial = factorial(self.n)
         self.entry_factorial.config(state="normal")
         self.entry_factorial.delete(0, tk.END)
-        self.entry_factorial.insert(0, str(factorial))
+        self.entry_factorial.insert(0, str(resultado_factorial))
         self.entry_factorial.config(state="readonly")
 
 if __name__ == "__main__":
