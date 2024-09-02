193df119a0d3c3e21b98165c0fafdfef6164fab3
Pablo Agustin Pena Heredia <107514101+pablopenaheredia@users.noreply.github.com>
2024-09-02 03:07:20 -0300

    Update Ejercicio1.2.py

--- Ejercicio1.2.py
+++ Ejercicio1.2.py
@@ -6,17 +6,17 @@ class ContDecreciente:
         self.ventana.title("Contador Decreciente")
         self.contador = 88
 
-        # Creamos los widgets
-        self.etiqueta = tk.Label(self.ventana, text="Contador")
+        # widgets
+        self.titulo = tk.Label(self.ventana, text="Contador")
         self.entry = tk.Entry(self.ventana, width=10)
         self.entry.insert(0, "88")
         self.entry.config(state="readonly")
-        self.boton = tk.Button(self.ventana, text="-", command=self.decrementar)
+        self.boton_restar = tk.Button(self.ventana, text="-", command=self.decrementar)
 
-        # Los colocamos en la ventana utilizando grid
-        self.etiqueta.grid(row=0, column=0, padx=5, pady=5)
+        # estructura
+        self.titulo.grid(row=0, column=0, padx=5, pady=5)
         self.entry.grid(row=0, column=1, padx=5, pady=5)
-        self.boton.grid(row=0, column=2, padx=5, pady=5)
+        self.boton_restar.grid(row=0, column=2, padx=5, pady=5)
 
         self.ventana.mainloop()
 
